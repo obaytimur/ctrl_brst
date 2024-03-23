5f7745298e58e3b092b787e13c98ffd8ce1a8e2c
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-23 13:23:37 +0300

    init: init project

---
+++ ctrl_brst/ctrl_brst/ctrl_brstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ctrl_brstApp.swift
+//  ctrl_brst
+//
+//  Created by Oğulcan Baytimur on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ctrl_brstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
